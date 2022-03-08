4b767fb4f48cafe9d6fb372f7ce194ae5d64a41e
tjscrib2012 <46504659+tjscrib2012@users.noreply.github.com>
2022-03-08 14:24:55 -0500

    Update intrusionHash.py
    Prototype 1. Should only print the names of files and directories.

--- intrusionHash.py
+++ intrusionHash.py
@@ -1,1 +1,23 @@
+import hashlib, os, glob
 
+
+#The following is a list of resourses used.
+#https://docs.python.org/3/library/os.html
+
+#ignore list
+ignore = []
+
+#root directory to start at
+rootdirectory = "/"
+
+#class declarations
+#Class to keep track of files hashed.
+class fileHash:
+  pass #will revise once more research
+
+#iterate through the different directories
+for x in glob.glob(rootdirectory,**,root_dir=rootdirectory,dir_fd=None,recursive=True):
+  if os.is_file(x):
+    print(x)
+  elif os.is_dir(x):
+    print(x)
